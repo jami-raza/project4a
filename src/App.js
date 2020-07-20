999ad410cbf02cb0c8e9242fddf34d508f0065a4
jami-raza <57630996+jami-raza@users.noreply.github.com>
2020-07-20 07:55:34 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Script from './Script';
+import './App.css';
+function App() {
+  return(
+    <div>
+      <Script />
+    </div>
+  )
+
+  } 
+
+export default App;
